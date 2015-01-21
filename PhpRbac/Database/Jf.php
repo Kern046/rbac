2ab407f8c693e0ea702e964e6e0194be2bee6785
Axel Venet <avenet@kilix.fr>
2015-01-21 10:34:59 +0100

    Fix sqlPdo method

--- PhpRbac/Database/Jf.php
+++ PhpRbac/Database/Jf.php
@@ -115,50 +115,38 @@ class Jf
 
 		$args = func_get_args ();
 
-		if (count ( $args ) == 1)
-		{
-			$result = self::$Db->query ( $Query );
-			if ($result===false)
-				return null;
-			$res=$result->fetchAll ( \PDO::FETCH_ASSOC );
-			if ($res===array())
-				return null;
-			return $res;
-		}
-		else
-		{
-			if (! $stmt = self::$Db->prepare ( $Query ))
-			{
-				return false;
-			}
-			array_shift ( $args ); // remove $Query from args
-			$i = 0;
-			foreach ( $args as &$v )
-				$stmt->bindValue ( ++ $i, $v );
+                if (! $stmt = self::$Db->prepare ( $Query )) {
+                        return false;
+                }
+                if (count ( $args ) > 1) {
+                    array_shift ( $args ); // remove $Query from args
+                    $i = 0;
+                    foreach ( $args as &$v ) {
+                        $stmt->bindValue ( ++ $i, $v );
+                    }
+                }
+                $success=$stmt->execute ();
 
-			$success=$stmt->execute ();
-
-			$type = substr ( trim ( strtoupper ( $Query ) ), 0, 6 );
-			if ($type == "INSERT")
-			{
-				if (!$success)
-					return null;
-				$res = self::$Db->lastInsertId ();
-				if ($res == 0)
-					return $stmt->rowCount ();
-				return $res;
-			}
-			elseif ($type == "DELETE" or $type == "UPDATE" or $type == "REPLACE")
-				return $stmt->rowCount();
-			elseif ($type == "SELECT")
-			{
-				$res=$stmt->fetchAll ( \PDO::FETCH_ASSOC );
-				if ($res===array())
-					return null;
-				else
-					return $res;
-			}
-		}
+                $type = substr ( trim ( strtoupper ( $Query ) ), 0, 6 );
+                if ($type == "INSERT")
+                {
+                        if (!$success)
+                                return null;
+                        $res = self::$Db->lastInsertId ();
+                        if ($res == 0)
+                                return $stmt->rowCount ();
+                        return $res;
+                }
+                elseif ($type == "DELETE" or $type == "UPDATE" or $type == "REPLACE")
+                        return $stmt->rowCount();
+                elseif ($type == "SELECT")
+                {
+                        $res=$stmt->fetchAll ( \PDO::FETCH_ASSOC );
+                        if ($res===array())
+                                return null;
+                        else
+                                return $res;
+                }
 	}
 
 	static function sqlMysqli( $Query)
